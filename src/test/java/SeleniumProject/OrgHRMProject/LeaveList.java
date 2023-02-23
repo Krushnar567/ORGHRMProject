7de4659afbd9d979268db077cbf724d4f96db127
Krushnar567 <115181197+Krushnar567@users.noreply.github.com>
2023-02-23 17:12:03 +0530

    Update LeaveList.java

--- src/test/java/SeleniumProject/OrgHRMProject/LeaveList.java
+++ src/test/java/SeleniumProject/OrgHRMProject/LeaveList.java
@@ -16,78 +16,79 @@ import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.AfterClass;
 
 public class LeaveList {
-  WebDriver driver;
-  @Test
-  public void LeaveList()  {
+WebDriver driver;
+@Test
+public void leaveList()  {
 	  
-	  WebElement userName = driver.findElement(By.xpath("//input[@name=\"username\"]"));
-	  userName.sendKeys("Admin");
+ WebElement userName = driver.findElement(By.xpath("//input[@name=\"username\"]"));
+ userName.sendKeys("Admin");
 	  
-	  WebElement password = driver.findElement(By.xpath("//input[@name=\"password\"]"));
-	  password.sendKeys("admin123");
+ WebElement password = driver.findElement(By.xpath("//input[@name=\"password\"]"));
+ password.sendKeys("admin123");
 	  
-	  WebElement loginButton = driver.findElement(By.tagName("button"));
-      loginButton.click();
+ WebElement loginButton = driver.findElement(By.tagName("button"));
+ loginButton.click();
       
-      WebElement leaveList = driver.findElement(By.xpath("//body/div[@id='app']/div[1]/div[1]/aside[1]/nav[1]/div[2]/ul[1]/li[3]/a[1]")) ;
-      leaveList.click();
+ WebElement leaveList = driver.findElement(By.xpath("//body/div[@id='app']/div[1]/div[1]/aside[1]/nav[1]/div[2]/ul[1]/li[3]/a[1]")) ;
+ leaveList.click();
       	  
-      }
+  }
       
-  @Test
-  public void assignLeave() {
+ @Test
+ public void assignLeave() {
 	  
-	  WebElement assignButton = driver.findElement(By.tagName("button"));
-	  assignButton.click();
+ WebElement assignButton = driver.findElement(By.tagName("button"));
+ assignButton.click();
 	  
-  }
+ }
 	  
-  @Test
-  public void nationality() {
-	  WebElement nationality = driver.findElement(By.xpath("//button[@type=\"button\"]"));
-	  nationality.click();
+@Test
+public void nationality() {
+WebElement nationality = driver.findElement(By.xpath("//button[@type=\"button\"]"));
+nationality.click();
 	  
-	  WebElement name = driver.findElement(By.xpath("//input[@class=\"oxd-input oxd-input--active\"]"));
+WebElement name = driver.findElement(By.xpath("//input[@class=\"oxd-input oxd-input--active\"]"));
 	  
-	  name.sendKeys("India");
+name.sendKeys("India");
 	  
-	  WebElement save =driver.findElement(By.xpath("//button[@type=\"submit\"]"));
-	  save.click();
+WebElement save =driver.findElement(By.xpath("//button[@type=\"submit\"]"));
+save.click();
 	  
-	boolean  verify = driver.findElement(By.id("app")).isDisplayed();
+boolean  verify = driver.findElement(By.id("app")).isDisplayed();
 	
-	if(verify==true) {
+if(verify==true) {
 		
-		System.out.println("Nationality is Selected");
-	}
+System.out.println("Nationality is Selected");
+}
 	
-	else{
+else{
 		
-		System.out.println("Nationality is not Selected");
+System.out.println("Nationality is not Selected");
 
-	}
+ }
 	  
-  }
-  @BeforeClass
-  public void beforeClass() {
+}
+@BeforeClass
+public void beforeClass() {
 	  
-	    WebDriverManager.chromedriver().setup();
+WebDriverManager.chromedriver().setup();
 
-		driver = new ChromeDriver();
+driver = new ChromeDriver();
 
-		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
 		
-		driver.manage().window().maximize();
+driver.manage().window().maximize();
 		
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
 		
-		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
-  }
+driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
+	
+}
 
-  @AfterClass
-  public void afterClass() {
+@AfterClass
+public void afterClass() {
 	  
-	  driver.close();
+driver.close();
 	  
 	  
   }
